80601efbef71f7955efaac01890606cb25eb8ccc
Kirt Morgan <kirtmorgan@live.com>
2019-05-05 15:51:54 +0100

    Listed out the methods the player class will have

--- player.py
+++ player.py
@@ -3,4 +3,16 @@ class Player:
         self.name = ''
         self.city = ''
         self.pokemon_caught_list = []
-    
+
+    def search_for_pokemon(self):
+        ''
+
+    def __try_catch_pokemon(self):
+        ''
+
+    def save_player_and_pokemon(self):
+        ''
+
+    def load_player_and_pokemon(self):
+        ''
+        
