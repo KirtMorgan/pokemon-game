8e93bd9c48b021628090eedacba75530fb848c56
Kirt Morgan <kirtmorgan@live.com>
2019-05-05 15:57:44 +0100

    Listed the methods for the pokemon class

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,18 @@
 class pokemon:
     def __init__(self):
         self.name = ''
+
+    def tackle(self):
+        ''
+
+    def make_noise(self):
+        ''
+
+    def rest_to_refill_health(self):
+        ''
+
+    def save_pokemon(self):
+        ''
+
+    def load_pokemon(self):
+        ''
